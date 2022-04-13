e13d53507ab8cc204190326a60202c06c5e4ad5e
JIdayyy <julien_abbadie@hotmail.fr>
2022-04-13 14:58:06 +0200

    feat: 🎸 widget button

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { useGetAllCategoriesQuery } from '../../generated/graphql'
 import CategoryCard from './Category.card'
+import './scrollbar.css'
 
 export default function Categories(): JSX.Element {
   const { data } = useGetAllCategoriesQuery()
   return (
-    <div className="h-full relative z-40 transform scrollbar-thin scrollbar scrollbar-track:rounded scrollbar-thumb-gray-300 scrollbar-track-gray-100 rounded-md -translate-y-8 w-11/12 overflow-y-auto flex flex-col align-middle justify-start py-2 px-4 bg-white">
+    <div className="h-full relative z-40 transform overflow-hidden scrollbar rounded-md -translate-y-8 w-11/12 overflow-y-auto flex flex-col align-middle justify-start py-2 px-4 bg-white">
       <span className="w-full text-left text-blue-base font-bold">
         Please select a category
       </span>
+
       {data?.categories.map((category) => (
         <CategoryCard category={category} key={category.id} />
       ))}
--- src/components/UI/ClosedWidget.button.tsx
+++ src/components/UI/ClosedWidget.button.tsx
@@ -1,5 +1,5 @@
 import React, { Dispatch, SetStateAction } from 'react'
-import WidgetButton from '../../static/widget_button.png'
+import WidgetButton from '../../static/Headset.png'
 
 type Props = {
   setIsOpen: Dispatch<SetStateAction<boolean>>
@@ -9,10 +9,11 @@ export default function ClosedWidgetButton({ setIsOpen }: Props): JSX.Element {
   return (
     <button
       type="button"
-      className="absolute bottom-10 right-10"
+      className="absolute bottom-10 right-10 flex items-center justify-around text-white bg-blue-base border-3 font-bold border rounded-md px-2 py-1 border-white"
       onClick={() => setIsOpen(true)}
     >
-      <img alt="widget_button" src={WidgetButton} width={50} height={50} />
+      <span>SUPPORT</span>
+      <img width={30} height={30} src={WidgetButton} alt="widget button" />
     </button>
   )
 }
